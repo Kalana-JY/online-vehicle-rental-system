45d4d6e3eea22e6a634f5ec127a329d4583616b2
Madusith752 <madusithdissanayake77@gmail.com>
2025-05-18 23:08:45 +0530

    vehicle
    display vehicle

--- src/main/java/com/rental/services/staffService.java
+++ src/main/java/com/rental/services/staffService.java
@@ -5,12 +5,15 @@ import java.sql.ResultSet;
 
 
 
-import java.sql.Statement;
 
+import java.sql.Statement;
+import java.util.ArrayList;
 
 import com.rental.model.*;
 
 import com.rental.utils.*;
+
+
 
 public class staffService {
 
@@ -99,6 +102,38 @@ public class staffService {
 			
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<vehicle> getAllVehicle(){
+		try {
+			ArrayList<vehicle>listVehi=new ArrayList<vehicle>();
+			
+			String query="select * from vehicle";
+			
+			Statement statement = DBConnect.getConnection().createStatement();
+			
+			ResultSet rs= statement.executeQuery(query);
+			while(rs.next()) {
+				vehicle vc=new vehicle();
+				vc.setVehicletype(rs.getString("vehicletype"));
+				vc.setTransmissiontype(rs.getString("transmissiontype"));
+				vc.setVehiclenumber(rs.getString("vehiclenumber"));
+				vc.setVehiclecolor(rs.getString("vehiclecolor"));
+				vc.setEnginenumber(rs.getString("enginenumber"));
+				vc.setSeatingcapacity(rs.getInt("seatingcapacity"));
+				vc.setFueltype(rs.getString("fueltype"));
+				vc.setVehiclephoto(rs.getBytes("vehiclephoto"));
+				listVehi.add(vc);
+			}
+			
+			return listVehi;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+			return null;
+			
 		}
 	}
 	
---
+++ src/main/java/com/rental/servlet/displayVehicle.java
@@ -0,0 +1,48 @@
+package com.rental.servlet;
+
+import java.io.IOException;
+
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.rental.model.*;
+import com.rental.services.*;
+
+
+@WebServlet("/displayVehicle")
+public class displayVehicle extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public displayVehicle() {
+        super();
+        
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		staffService service=new staffService();
+		
+		ArrayList<vehicle> vehicle=service.getAllVehicle();
+		
+		request.setAttribute("vehicle",vehicle);
+		
+		RequestDispatcher dispacher=request.getRequestDispatcher("Staff.jsp");
+		
+		dispacher.forward(request, response);
+	}
+
+}
